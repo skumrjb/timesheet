8e6fa8b16bb47fdc3bad60e927adaff91efd87be
C.K. Santosh <CKS@Santosh-CK.local>
2020-10-21 00:22:30 +0400

    Timesheet update-Rev1

--- timesheet.py
+++ timesheet.py
@@ -1,20 +1,3 @@
-"""
-Goal of the timesheet is to ensure I have reached 4 hours of focussed productive work workflow:
-
-Aim - Open the terminal and run the script to note the start of the clock
-
-2 - User types in the prompt - 'in' to start the clock details of the day and time are stored in the .csv
-
-3 - User types in the prompt - 'end' end the app records the timeout
-
-this action will also record the total time of the session in a separate column
-
-4 - if the clock is started later in the day - the timesheet should be able to do steps 2 and 3
-
-5 - at the end of the week (Sunday) a graph is plotted against day of the week and number of hours spent - incomplete
-
-the results are saved in the directory as a jpg and sent as an attachment to my email id - incomplete
-"""
 
 import datetime
 import csv
