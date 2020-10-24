904ab854aecf30ea134c514327c856ed25b661ef
C.K. Santosh <CKS@Santosh-CK.local>
2020-10-24 23:27:28 +0400

    added few functions and expanding the stats

--- timesheet.py
+++ timesheet.py
@@ -1,11 +1,11 @@
-
 import datetime
 import csv
 import os.path
+import pandas as pd
 
 Username = input('Please enter the username: ')
 print('Please enter the timesheet code to begin')
-action = input('Punch (in/out): ')
+action = input('Punch (in/stats): ')
 
 if action == 'in':
 
@@ -30,6 +30,19 @@ if action == 'in':
              'End Time': str(clock_time.hour) + ':' + str(clock_time.minute) + ':' + str(clock_time.second),
              'Total Time': str(total.seconds)})
 
+
+    def goal_reached(time):
+        clock_gse = time.now()
+        date = clock_gse.strftime('%d')
+        day_date = clock_gse.strftime('%Y/%m/%d')
+        data_gse = pd.read_csv('timesheet_log.csv')
+        df_gse = pd.DataFrame(data_gse)
+        total_gse = df_gse.loc[df_gse['Date'] == f'{date}', 'Total Time'].sum()
+        if total_gse >= 14400:
+            print(f'Congrats you have reached the goal for{day_date}')
+
+        else:
+            print(f'You have not reached the 4 hour goal yet! on {day_date}')
 
     """
     Displaying program's instructions and collecting the username and action
@@ -61,16 +74,36 @@ if action == 'in':
                 for rows in timesheet_log:
                     writer.writerow(rows)
 
+            goal_reached(clock)
+
         elif action2 == action:
             print('You cannot have duplicate punch-ins')
             punch_out(clock)
+            goal_reached(clock)
 
         else:
             print('Your punch-out code is incorrect')
             punch_out(clock)
+            goal_reached(clock)
 
     except KeyboardInterrupt:
         punch_out(clock)
+        goal_reached(clock)
+
+elif action == 'stats':
+
+    data = pd.read_csv('timesheet_log.csv')
+    df = pd.DataFrame(data)
+
+    status_options = input('Please enter your option : \n' +
+                           '1. date for :   Status by date \n' +
+                           '2. month for :  Status by month \n' +
+                           '3. year for :   Status by year\n')
+    if status_options == 'date':
+        stats_for_date = input('Enter the date in yyyy-mm-dd format for the stats: ')
+
+        total = df.loc[df['Date'] == f'{stats_for_date}', 'Total Time'].sum()
+        print(f'you have clocked {total / 60} minutes')
 
 else:
     raise Exception('Please enter a valid response')
